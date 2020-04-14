b2b7a01552515b2f4afdc4700c3b55723c10d955
alexandrosnic <alexnic31@gmail.com>
2020-04-14 16:16:39 +0200

    First commit

---
+++ lanes.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Greyscale
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    canny = cv2.Canny(blur, 50,150)
+    return canny
+
+# Region of interest
+def region_of_interest(image):
+    height = image.shape[0] # to know the height
+    polygons = np.array([
+    [(200, height), (1100,height), (550,250)]
+    ]) # our array of polygons
+    mask = np.zeros_like(image) # creates an array of zeros with the same shape as image
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+# Display the lines
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1,y1), (x2,y2), (255, 0, 0), 10)
+    return line_image
+
+# Make x, y coordinates
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
+# Average of the lines
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        # We get the parameters m and b of a one-order line to fit these 2 points
+        parameters = np.polyfit((x1,x2), (y1,y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        # y is reversed => left line has negative slope
+        if slope<0:
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
+    # Then we get the average slope
+    left_fit_average = np.average(left_fit, axis = 0)
+    right_fit_average = np.average(right_fit, axis = 0)
+    # We found the parameters but then we need to translate them into x and y
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
+# For detecting line edges in an image
+image = cv2.imread('test_image.jpg')
+# Attention: Copy instead of using exact the same image
+lane_image = np.copy(image)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+averaged_lines = average_slope_intercept(lane_image, lines)
+cv2.imshow('result', display_lines(cropped_image, averaged_lines))
+cv2.waitKey(0)
+line_image = display_lines(lane_image, averaged_lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+# cv2.imshow('result', combo_image)
+# cv2.waitKey(0)
+
+# For detecting line edges in a video capture
+# cap = cv2.VideoCapture("test2.mp4")
+# while(cap.isOpened()):
+#     _, frame = cap.read() # returns a boolean we are not interested now, and the frame
+#     canny_image = canny(frame)
+#     cropped_image = region_of_interest(canny_image)
+#     lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+#     averaged_lines = average_slope_intercept(frame, lines)
+#     line_image = display_lines(frame, averaged_lines)
+#     combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+#     cv2.imshow('result', combo_image)
+#
+#     # Displays the image for a specific amount of time
+#     # With 1 it will wait 1ms in between the frames. Ιf we press q, then break
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+#
+# cap.release()
+
+# # Alternatively:
+# plt.imshow(canny)
+# plt.show()
+
+cv2.destroyAllWindows()
